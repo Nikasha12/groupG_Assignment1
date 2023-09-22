839f163b3117c28dd10592ee82402a00791a3e6c
Deepbhardwaj <141149121+Deepbhardwaj@users.noreply.github.com>
2023-09-21 19:50:57 -0500

    passwordGenerator Function

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	fmt.Println("Hi")
-        fmt.Println(rnumberIncrement()
-        fmt.Println(multliply))
+        fmt.Println(rnumberIncrement())
+        fmt.Println(multliply())
+	fmt.Println(passwordGenerator())
 }
 
 func numberIncrement(){
@@ -19,4 +23,30 @@ func multiply () {
 	result := multiply(5, 7)
 	return a * b
 	fmt.Println("Result:", result)
+}
+
+
+// defined a function GenerateRandomPassword generates a random password of the specified length.
+func GenerateRandomPassword(length int) string {
+
+	
+	// Defined the data set from the password should generate
+	charset := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+
+	// Create a password by randomly selecting characters from the charset
+	password := make([]byte, length)
+	for i := range password {
+		password[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(password)
+}
+
+func passwordGenerator() {
+	passwordLength := 16 // Change this to your desired password length
+
+	// Generate a random password
+	password := GenerateRandomPassword(passwordLength)
+
+	fmt.Println("Generated Password:", password)
 }
