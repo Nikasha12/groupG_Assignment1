5ee540218e90dd99cb30b014c5f7e4ed04b5bafb
Mayankanand1 <145071227+Mayankanand1@users.noreply.github.com>
2023-09-21 20:32:23 -0400

    increment function

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hi")
+        fmt.Println(rnumberIncrement())
 }
+
+func numberIncrement(){
+	initialNumber := 5
+	incrementedNumber := increment(initialNumber)
+
+	fmt.Printf("The incremented number is: %d\n", incrementedNumber)
+} 
